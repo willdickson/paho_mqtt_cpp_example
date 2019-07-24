f2bb3de96735c697be357954153c72db160b647a
Will Dickson <wbd@caltech.edu>
2019-07-24 16:14:08 -0700

    Added attribution to main.cpp.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,31 @@
+// sync_publish.cpp
+//
+// This is a Paho MQTT C++ client, sample application.
+//
+// It's an example of how to send messages as an MQTT publisher using the
+// C++ synchronous client interface.
+//
+// The sample demonstrates:
+//  - Connecting to an MQTT server/broker
+//  - Publishing messages
+//  - User-defined persistence
+//
+
+/*******************************************************************************
+ * Copyright (c) 2013-2017 Frank Pagliughi <fpagliughi@mindspring.com>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ *
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ *   http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *    Frank Pagliughi - initial implementation and documentation
+ *******************************************************************************/
 #include <sstream>
 #include <iostream>
 #include <cstdlib>
